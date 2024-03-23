0200c92a4b21d67f46f0ff29f4ae3d328e6778d2
abhisheksooni <abhisheksoni.as444@gmail.com>
2024-03-23 23:11:34 +0530

    build: Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,47 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { img1,img2,img3,img4,img5,img6,img7 } from './Imports/Images.js'
 
 function App() {
+
+  const skills = [
+   { img:img1},
+   { img:img2},
+   { img:img3},
+   { img:img4},
+   { img:img5},
+   { img:img6},
+   { img:img7},
+  ]
+
   return (
     <>
-      <h1>Hello somya </h1>
+      <section className='text-white flex justify-center items-center gap-10 lg:gap-20 my-20 flex-wrap max-w-[1800px]'>
+          <div className="text-center lg:text-start">
+            <h1 className='text-5xl poppins-bold'>I'm Somya Soni,</h1>
+            <h1 className='text-5xl poppins-bold py-1.5'>Interion Designer</h1>
+            <p className='max-w-[500px] my-2.5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quam excepturi similique error optio amet quis reprehenderit eum</p>
+            <div className="my-4">
+              <button className='bg-white text-color1 font-semibold py-2 px-6 rounded-full mr-3'>About</button>
+              <button className='bg-color3 text-white font-semibold py-2 px-6 rounded-full'>Download CV</button>
+            </div>
+          </div>
+          <div className="w-[320px]">
+            <img className='bg-color3 w-full h-[450px] rounded-3xl' src="" alt="" />
+          </div>
+      </section>
+      {/* Skills */}
+      <section className=' bg-color2 text-center pt-5 pb-16 text-white'>
+      <p className='text-2xl font-semibold my-4 mb-6'>My Skills</p>
+      <div className=" flex justify-center flex-wrap px-5 md:px-0 gap-5 hover:*:scale-125">
+        {
+          skills.map((i)=>(
+            <img key={i} className='max-w-[60px] ' src={i.img} alt="" />
+          ))
+        }
+      </div>
+      </section>
     </>
   )
 }
---
+++ src/Imports/Images.js
@@ -0,0 +1,18 @@
+import img1 from '../Images/skills_icons/ai.png'
+import img2 from '../Images/skills_icons/autodesk.png'
+import img3 from '../Images/skills_icons/Enscape.png'
+import img4 from '../Images/skills_icons/lumion.png'
+import img5 from '../Images/skills_icons/ps.png'
+import img6 from '../Images/skills_icons/sketchbook.webp'
+import img7 from '../Images/skills_icons/sketchup.png'
+
+
+export{
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+}
