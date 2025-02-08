5a22650bd342582fb71fe9efba778137ecbc06f7
abhishek soni <abhisheksoni.as444@gmail.com>
2025-02-08 18:23:28 +0530

    project page add

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,22 +2,36 @@ import React, { useEffect } from 'react'
 
 function Projects() {
 
-  useEffect(()=>{
-      window.scroll({top:'0',behavior:'smooth'}) 
-  },[])
+  useEffect(() => {
+    window.scroll({ top: '0', behavior: 'smooth' })
+  }, [])
 
   return (
 
-   
     <>
-    <div className="h-[60px] lg:h-[120px] px-4"></div>
-    <section className="flex justify-center items-center flex-col text-white">
-    <div className=" flex items-center gap-5 ">
-                <div className="bg-white w-[7px] h-[7px] rounded-full"></div>
-                <p className='py-12 font-semibold text-3xl'>Projects</p>
-                <div className="bg-white w-[7px] h-[7px] rounded-full"></div>
-            </div>
-    </section>
+      {/* <div className="h-[60px] lg:h-[120px] px-4"></div> */}
+      <section className="w-[80%] mx-auto  mb-16">
+      
+<p className='text-5xl font-semibold '>Project</p>
+<div className=""></div>
+        {/* button */}
+        <div className="*:bg-color4 py-8 flex gap-3 *:py-1.5 *:px-4 *:rounded-full mt-5">
+          <button>All</button>
+          <button>Room</button>
+          <button>Bathroom</button>
+        </div>
+        
+        {/* projects */}
+        <section className=' flex flex-wrap gap-8 *:rounded-lg '>
+<div className="w-72 h-72 bg-color2"></div>
+<div className="w-72 h-72 bg-color2"></div>
+<div className="w-72 h-72 bg-color2"></div>
+<div className="w-72 h-72 bg-color2"></div>
+<div className="w-72 h-72 bg-color2"></div>
+<div className="w-72 h-72 bg-color2"></div>
+<div className="w-72 h-72 bg-color2"></div>
+        </section>
+      </section>
     </>
   )
 }
