55d1bf75e12fa9143f3b119eb08f90b6fa32c56b
abhishek soni <abhisheksoni.as444@gmail.com>
2025-02-09 16:29:37 +0530

    ui update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -10,26 +10,31 @@ function Projects() {
 
     <>
       {/* <div className="h-[60px] lg:h-[120px] px-4"></div> */}
-      <section className="w-[80%] mx-auto  mb-16">
+      <section className="w-[80%] mx-auto flex flex-col items-center  mb-16">
       
 <p className='text-5xl font-semibold '>Project</p>
 <div className=""></div>
         {/* button */}
-        <div className="*:bg-color4 py-8 flex gap-3 *:py-1.5 *:px-4 *:rounded-full mt-5">
-          <button>All</button>
-          <button>Room</button>
-          <button>Bathroom</button>
+        <div className="hover:*:bg-color4 *:duration-300  *:bg-white py-8 flex gap-3 *:py-1.5 *:px-4 *:rounded-full mt-5">
+          <button className='flex items-center justify-center gap-3'> <img src="https://static.havenly.com/home/before-and-after/living.svg" alt="" /> <p>Living Room</p></button>
+          <button className='flex items-center justify-center gap-3'> <img src="https://static.havenly.com/home/before-and-after/dining.svg" alt="" /> <p>Dining Room</p></button>
+          <button className='flex items-center justify-center gap-3'> <img src="https://static.havenly.com/home/before-and-after/bedroom.svg" alt="" /> <p>Bedroom</p></button>
+          <button className='flex items-center justify-center gap-3'> <img src="https://static.havenly.com/home/before-and-after/more.svg" alt="" /> <p>Explore More</p></button>
         </div>
         
         {/* projects */}
-        <section className=' flex flex-wrap gap-8 *:rounded-lg '>
-<div className="w-72 h-72 bg-color2"></div>
-<div className="w-72 h-72 bg-color2"></div>
-<div className="w-72 h-72 bg-color2"></div>
-<div className="w-72 h-72 bg-color2"></div>
-<div className="w-72 h-72 bg-color2"></div>
-<div className="w-72 h-72 bg-color2"></div>
-<div className="w-72 h-72 bg-color2"></div>
+        <section className=' flexs flex-wrap  *:rounded-lg '>
+          <div className="flex gap-5  *:rounded-lg">
+<div className="w-[700px] h-[395px] bg-color2"></div>
+<div className="w-[280px] h-[395px] bg-color2"></div>
+<div className="w-[280px] h-[395px] bg-color2"></div>
+          </div>
+          <div className="flex gap-5 mt-4 *:rounded-lg">
+<div className="w-[280px] h-[395px] bg-color2"></div>
+<div className="w-[280px] h-[395px] bg-color2"></div>
+<div className="w-[700px] h-[395px] bg-color2"></div>
+          </div>
+
         </section>
       </section>
     </>
