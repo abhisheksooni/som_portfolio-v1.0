d0089de678cb035756dc6641614b7c6a0734d089
abhishek soni <abhisheksoni.as444@gmail.com>
2025-03-04 23:13:37 +0530

    services style fix

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,22 +2,41 @@ import React from 'react'
 
 function Services() {
     return (
-        <section className='flex w-full justify-center gap-5  text-lg items-center '>
-            <div className="w-[50%]  ">
+        <section className=' px-5 md:px-0 md:flex w-full justify-center gap-5  text-lg items-center '>
+            <div className="md:w-[47%]    lg:h-[80vh] mb-10 md:mb-0 mt-5 md:mt-0">
                 <p className='text-3xl text-center mb-5 uppercase'>our Process</p>
-                <ul className="  p-10  bg-color4 rounded-xl list-disc list-inside *:mb-5">
-                    <li>Understanding Client Vision – We begin by discussing your ideas, preferences, and requirements to create a design concept that aligns with your vision.</li>
+                
+                <div className="  p-10  bg-color4 rounded-xl list-disc list-inside *:mb-5">
 
-                    <li>Concept Development – Our team refines the initial ideas into structured design concepts, ensuring functionality and aesthetics.</li>
+                <div className="flex gap-4 items-start ">
+                        <div className="bg-black p-1 h-1 mt-2 w-1 rounded-full"></div>
+                        <p><span className='font-bold'>Understanding Client Vision</span> <br />
+                        Our team refines the initial ideas into structured design concepts, ensuring functionality and aesthetics</p>
+                    </div>
+                <div className="flex gap-4 items-start ">
+                        <div className="bg-black p-1 h-1 mt-2 w-1 rounded-full"></div>
+                        <p><span className='font-bold'>Concept Development</span> <br />
+                        We begin by discussing your ideas, preferences, and requirements to create a design concept that aligns with your vision.</p>
+                    </div>
+                <div className="flex gap-4 items-start ">
+                        <div className="bg-black p-1 h-1 mt-2 w-1 rounded-full"></div>
+                        <p><span className='font-bold'>2D Drawings & Space Planning </span> <br />
+                        We create detailed 2D layouts, including furniture placement, material selections, and design specifications to guide the overall concept.</p>
+                    </div>
+                <div className="flex gap-4 items-start ">
+                        <div className="bg-black p-1 h-1 mt-2 w-1 rounded-full"></div>
+                        <p><span className='font-bold'>3D Visualizations & Renders</span> <br />
+                        We transform the concept into high-quality 3D visuals, providing a realistic preview of the final design. This helps clients make informed decisions before any physical work begins.</p>
+                    </div>
+                <div className="flex gap-4 items-start ">
+                        <div className="bg-black p-1 h-1 mt-2 w-1 rounded-full"></div>
+                        <p><span className='font-bold'>Client Feedback & Revisions</span> <br />
+                        We collaborate closely with clients, making necessary adjustments to ensure the final renderings align perfectly with their expectations.</p>
+                    </div>
 
-                    <li>2D Drawings & Space Planning – We create detailed 2D layouts, including furniture placement, material selections, and design specifications to guide the overall concept.</li>
-
-                    <li>3D Visualizations & Renders – We transform the concept into high-quality 3D visuals, providing a realistic preview of the final design. This helps clients make informed decisions before any physical work begins.</li>
-
-                    <li>Client Feedback & Revisions – We collaborate closely with clients, making necessary adjustments to ensure the final renderings align perfectly with their expectations.</li>
-                </ul>
+                </div>
             </div>
-            <div className="w-[50%] ">
+            <div className="md:w-[47%]  lg:h-[80vh]  mb-10 md:mb-0">
 
                 <p className='text-3xl mb-5 text-center uppercase'>Services</p>
                 <div className="bg-color4 p-10 rounded-xl list-disc list-inside *:mb-5">
@@ -40,21 +59,30 @@ function Services() {
                             Space optimization strategies for efficient layouts</p>
 
                     </div>
+                    <div className="flex gap-4 items-start ">
+                        <div className="bg-black p-1 h-1 mt-2 w-1 rounded-full"></div>
+                        <p> <span className='font-bold'>2D Drawings & Space Planning</span>  <br />
 
-                    <p>2D Drawings & Space Planning <br />
+                            Floor plans, elevations, and detailed layouts
 
-                        Floor plans, elevations, and detailed layouts
+                            Furniture placement and material specifications
 
-                        Furniture placement and material specifications
+                            CAD drawings for precise execution</p>
 
-                        CAD drawings for precise execution</p>
-                    <p>Color & Material Selection <br />
+                    </div>
+                    <div className="flex gap-4 items-start ">
+                        <div className="bg-black p-1 h-1 mt-2 w-1 rounded-full"></div>
+                        <p> <span className='font-bold'>Color & Material Selection</span>  <br />
 
-                        Guidance on the best material combinations for a cohesive look
+                            Guidance on the best material combinations for a cohesive look
 
-                        Selection of paint colors, textures, and finishes
+                            Selection of paint colors, textures, and finishes
 
-                        Coordination with suppliers for material sourcing</p>
+                            Coordination with suppliers for material sourcing</p>
+
+                    </div>
+
+
 
                 </div>
             </div>
