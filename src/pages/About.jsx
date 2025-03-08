85fc79f3be990d699318d86115a024ce19f98eb8
abhishek soni <abhisheksoni.as444@gmail.com>
2025-03-08 22:20:26 +0530

    about text add

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-
+import { project1, som, t1, t2, t3 } from '../Imports/Images'
 function About() {
 
   useEffect(()=>{
@@ -7,28 +7,25 @@ function About() {
   },[])
   return (
     <>
-    <div className="h-[90px] lg:h-[120px]"></div>
-      <section className='max-w-[1800px] mx-auto px-4'>
-        <div className=" flex items-center justify-center text-white gap-5">
-          <div className="bg-white w-[7px] h-[7px] rounded-full"></div>
-          <p className='py-6 font-semibold text-3xl'>  About Us</p>
-          <div className="bg-white w-[7px] h-[7px] rounded-full"></div>
+    <div className="h-[40px] lg:h-[100px]"></div>
+      <section className='max-w-[1800px] mx-auto lg:h-[70vh] px-4'>
+        <div className=" flex items-center justify-center text-color3 gap-5">
+          {/* <div className="bg-white w-[7px] h-[7px] rounded-full"></div> */}
+          <p className='py-3 font-semibold text-5xl'>  About Us</p>
+          {/* <div className="bg-white w-[7px] h-[7px] rounded-full"></div> */}
         </div>
 
-        <div className=' text-white flex justify-center items-center gap-10 lg:gap-20 my-20 flex-wrap '>
+        <div className=' text-color3 flex justify-center items-center gap-10 lg:gap-20 my-20 flex-wrap '>
 
 {/* h-[450px] */}
           <div className="w-[320px]">
-            <img className='bg-color3 w-full rounded-3xl' src="" alt="" />
+            <img className='bg-color3 w-full rounded-3xl' src={som} alt="" />
           </div>
           <div className="text-center lg:text-start">
-            <h1 className='text-5xl poppins-bold'>I'm Somya Soni,</h1>
+            <h1 className='text-5xl poppins-bold'>I'm Som Soni,</h1>
             <h1 className='text-5xl poppins-bold py-1.5'>Interion Designer</h1>
-            <p className='max-w-[500px] my-2.5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quam excepturi similique error optio amet quis reprehenderit eum</p>
-            {/* <div className="my-4">
-              <button className='bg-white text-color1 font-semibold py-2 px-6 rounded-full mr-3'>About</button>
-              <button className='bg-color3 text-white font-semibold py-2 px-6 rounded-full'>Download CV</button>
-            </div> */}
+            <p className='max-w-[500px] my-2.5'>At AS Renderings, we are a team of passionate interior designers and 3D visualizers dedicated to transforming spaces into exceptional living and working environments. We specialize in 3D visualization, detailed space planning, and color selection to bring your ideas to life. Our commitment is to deliver designs that blend creativity with functionality, ensuring each project reflects a unique character and style.</p>
+
           </div>
         </div>
       </section>
