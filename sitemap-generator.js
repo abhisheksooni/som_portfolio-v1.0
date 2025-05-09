14d62fc7fd20e9922a6940fb01e98204cb7767da
abhishek soni <abhisheksoni.as444@gmail.com>
2025-05-09 21:12:50 +0530

    seo set in

---
+++ sitemap-generator.js
@@ -0,0 +1,16 @@
+// sitemap-generator
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+
+(async () => {
+  const sitemap = new SitemapStream({ hostname: 'https://www.asarastudio.in' });
+  const writeStream = createWriteStream('./public/sitemap.xml');
+  sitemap.pipe(writeStream);
+
+  sitemap.write({ url: '/', changefreq: 'monthly', priority: 1.0 });
+  sitemap.write({ url: '/services', changefreq: 'monthly' });
+  sitemap.write({ url: '/faq', changefreq: 'monthly' });
+  sitemap.end();
+
+  await streamToPromise(sitemap);
+})();
